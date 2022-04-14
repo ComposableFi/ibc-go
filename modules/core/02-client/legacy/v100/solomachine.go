261930e96445dc18b503dd59b46d54fa8f457395
Thomas Modeneis <thomas.modeneis@gmail.com>
2022-04-14 21:11:46 +0200

    refactoring CheckHeaderAndUpdateState into VerifyClientMessage, CheckForMisbehaviour, UpdateStateOnMisbehaviour and UpdateState

--- modules/core/02-client/legacy/v100/solomachine.go
+++ modules/core/02-client/legacy/v100/solomachine.go
@@ -107,8 +107,8 @@ func (cs *ClientState) VerifyClientMessage(
 	panic("legacy solo machine is deprecated!")
 }
 
-// UpdateState panis!
-func (cs *ClientState) UpdateState(_ sdk.Context, _ codec.BinaryCodec, _ sdk.KVStore, _ exported.ClientMessage) []exported.Height {
+// UpdateState panics!
+func (cs *ClientState) UpdateState(_ sdk.Context, _ codec.BinaryCodec, _ sdk.KVStore, _ exported.ClientMessage) error {
 	panic("legacy solo machine is deprecated!")
 }
 
