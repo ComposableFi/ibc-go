efa1b22a3c2f933a2495bdb719f83e3c9cb4a3e4
Vladislav Markushin <negigic@gmail.com>
2023-02-06 17:58:21 +0400

    build_local_network.sh

--- build_local_network.sh
+++ build_local_network.sh
@@ -9,8 +9,9 @@ simd init $MONIKER --chain-id ibcgo-1
 
 simd keys add k1 --keyring-backend test
 
-VALIDATOR_ADDRESS=$(simd keys show k1 -a --keyring-backend test)
+VALIDATOR_ADDRESS=$(simd keys show k1 -a --keyring-backend test) # cosmos1z5a2mrllta4yhkwef7vjqkcf9pwwdg6lx9ajhz
 LOCAL_ADDRESS="cosmos1nnypkcfrvu3e9dhzeggpn4kh622l4cq7wwwrn0"
+# 0x636f736d6f73316e6e79706b636672767533653964687a656767706e346b683632326c34637137777777726e30
 
 simd add-genesis-account $VALIDATOR_ADDRESS 100000000000000stake
 simd add-genesis-account $LOCAL_ADDRESS 100000000000stake
